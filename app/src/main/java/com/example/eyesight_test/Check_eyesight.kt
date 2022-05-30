6c5f7ef10e6c565771ae6516051bd3e39dfb149c
rlawogns <rlawogns0411@naver.com>
2022-05-30 15:21:40 +0900

    feat : main function

---
+++ app/src/main/java/com/example/eyesight_test/Check_eyesight.kt
@@ -0,0 +1,12 @@
+package com.example.eyesight_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Check_eyesight : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_eyesight)
+
+    }
+}
--- app/src/main/java/com/example/eyesight_test/MainActivity.kt
+++ app/src/main/java/com/example/eyesight_test/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.eyesight_test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eyesight_test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mBinding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding!!.root)
+        mBinding!!.button1.setOnClickListener(){
+            val intent = Intent(this, Check_eyesight::class.java)
+            startActivity(intent)
+        }
+        mBinding!!.button2.setOnClickListener(){
+            val intent = Intent(this, Check_colorblindness::class.java)
+            startActivity(intent)
+        }
     }
 }
